f31b626f8b82612a927d68de5f4630c4746a21c5
MidKnight92 <MidKnight92@users.noreply.github.com>
2020-01-22 03:57:33 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -12,14 +12,14 @@ int main(void)
     {
         //cols       
         //spaces: total number of rows (n) aka the height minus the actual row number(i)
-       for (int j = 1; j <= (n - i); j++)
-       {
+        for (int j = 1; j <= (n - i); j++)
+        {
             printf(" ");
-       }
-       for (int k = 1; k <= i; k++)
-       {
+        }
+        for (int k = 1; k <= i; k++)
+        {
             printf("#");
-       }
+        }
         printf("\n");
     }     
 }
