237446a5bf6bfef08121b757a960f324238c2b5e
MidKnight92 <MidKnight92@users.noreply.github.com>
2020-01-25 23:00:24 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -1,3 +1,5 @@
+// Determines grade level of inputed text
+
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
@@ -19,6 +21,8 @@ int main(void)
 
     prompt();
     float real_num = 0.0588 * L - 0.296 * S - 15.8;
+
+    // Round float to int
     int index = roundf(real_num);
 
     if (index >= 16)
@@ -35,7 +39,7 @@ int main(void)
     }
 }
 
-//prompt user for a string of text
+// Prompt user for a string of text
 string prompt(void)
 {
     string text;
@@ -49,67 +53,60 @@ string prompt(void)
 }
 
 
-
+// Determine the number of letters, words, and sentences in the text
 int count_args(string text)
 {
+    int letters = 0, words = 1, sentences = 0;
+    char p = '.', q = '?', e = '!';
 
-   int letters = 0;
-   int words = 1;
-   int sentences = 0;
-   char p = '.';
-   char q = '?';
-   char e = '!';
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        // If a lowercase or uppercase letter is detected increment letters
+        if (islower(text[i]) || isupper(text[i]))
+        {
+            letters++;
+        }
 
+        // If a space is detected increment words and add one for the last word in the sentence
+        if (isspace(text[i]))
+        {
+            words++;
+        }
 
-   for (int i = 0, n = strlen(text); i < n; i++)
-   {
-       if (islower(text[i]) || isupper(text[i]))
-       {
-           letters++;
-       }
+        // If '.', '!', or '?' is detected increment sentences
+        if (text[i] == p || text[i] == q || text[i] == e)
+        {
+            sentences++;
+        }
+    }
+    // Pass the ints of letters and words as args to avg_ltrs
+    avg_ltrs(letters, words);
 
-       if (isspace(text[i]))
-       {
-           words++;
-       }
+    // Pass the ints of letters and words as args to avg_sent
+    avg_sent(sentences, words);
 
-       if (text[i] == p || text[i] == q || text[i] == e)
-       {
-            // printf("This is text[i]:%c p:%c q:%c e:%c\n", text[i], p, q, e);
-           sentences++;
-       }
-   }
-//   printf("This the letters, words, sentence count: %i %i %i\n", letters, words, sentences);
-
-   avg_ltrs( letters, words);
-   avg_sent(sentences, words);
-
-   return 0;
+    return 0;
 }
 
-// TODO
-// avg number of letters per 100 words in the text
+
+// Average number of letters per 100 words in the text
 int avg_ltrs(int letter, int words)
 {
     float l = 0;
 
-
     l = (letter * 100.0) / words;
-
-    // printf("L:%.1f\n", l);
+    //redefine the global variable
     L = l;
     return 0;
 }
 
-// //avg number of sentences per 100 words in the text
+// Average number of sentences per 100 words in the text
 int avg_sent(int sentences, int words)
 {
     float s = 0;
 
-
     s = (sentences * 100.0) / words;
-
-    // printf("S: %.1f\n", s);
+    // Redefine the global variable
     S = s;
     return 0;
 }
