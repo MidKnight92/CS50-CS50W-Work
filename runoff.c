81749aa75ddceb6f51e40316e5f8f9f190e02d4f
MidKnight92 <MidKnight92@users.noreply.github.com>
2020-02-05 03:55:24 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -179,17 +179,21 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    int min = 1;
+    int min = 0;
     for (int i = 0; i < candidate_count; i++)
     {
-        if (!candidates[i].eliminated)
+        if (!candidates[i].eliminated && candidates[i].votes == min)
         {
-            if (min >= candidates[i].votes)
-            {
-                min = candidates[i].votes;
-            }
+            // printf("%d\n", min);
+            // printf("This is candidate%s:%d\n", candidates[i].name, candidates[i].votes);
+            return min;
+        }
+        else
+        {
+             min++;
+        }
     }
-    return min;
+    return 0;
 }
 
 // Return true if the election is tied between all candidates, false otherwise
