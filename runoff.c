b68e8b329d1c31c5c5720baeee972d8200d4167c
MidKnight92 <MidKnight92@users.noreply.github.com>
2020-02-05 03:28:47 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -163,11 +163,11 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    int majority = (voter_count * .5) + 1;
+    float majority = (voter_count / 2) ;
     // printf("%d", majority);
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes == majority)
+        if (candidates[i].votes >= majority)
         {
             printf("%s\n",candidates[i].name);
             return true;
