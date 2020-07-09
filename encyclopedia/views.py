a087dbba0c0f6498021133aa3a64e775cc3deb01
Stephanie Viveros <sviveros05@gmail.com>
2020-07-09 01:00:52 -0500

    Fix Import Error and Run Migrations

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 from . import util
 
-from util import get_entry, list_entries, save_entry
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -15,7 +14,7 @@ def entry(request, title):
         The view should get the content of the encyclopedia entry by calling the appropriate util function.
         If an entry is requested that does not exist, the user should be presented with an error page indicating that their requested page was not found.
         If the entry does exist, the user should be presented with a page that displays the content of the entry. The title of the page should include the name of the entry."""
-    entry = get_entry(title)
+    entry = util.get_entry(title)
     if entry:
         print(entry)
         return(render, "encyclopedia/entry.html", {
