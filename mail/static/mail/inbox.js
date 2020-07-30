921b5bd68d101e225dea384714ad23a660b1e70d
Stephanie Viveros <sviveros05@gmail.com>
2020-07-29 20:32:27 -0500

    Fetch and Display Emails from Mailboxes

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,16 +1,47 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
-
   
+
   // By default, load the inbox
   load_mailbox('inbox');
 });
+
+
+
+async function get_emails(mailbox){
+  try {
+    await fetch(`/emails/${mailbox}`)
+    .then(response => response.json())
+    .then(result => {
+      // Show fetched messages
+      if (result.length !== 0){
+          result.forEach(email => {
+          const element = document.createElement('div');
+          element.innerHTML = `<b>Sender:</b> ${email['sender']} <br/><b>Subject:</b>${email['subject']} <br/> <b>Message:</b> ${email['body']} <br/><b>Time:</b> ${email['timestamp']}`;
+          element.style = "border: solid 2px dodgerBlue; background-color: GhostWhite; padding: 1%; margin-bottom: 4%;";
+          document.querySelector('#emails-view').append(element)
+        });
+      // No messages to fetch relay that to user  
+      } else {
+        const div = document.createElement('div');
+        div.innerHTML = `<p>No messages</p>`
+        document.querySelector('#emails-view').append(div)
+      }
+    });
+
+  } catch (error) {
+    // Create an error message and append it to the emails-view
+    const div = document.createElement('div');
+    div.innerHTML = `<p>Sorry there was an error</p>`
+    document.querySelector('#emails-view').append(div)
+    console.log(error);
+  }
+}
 
 function compose_email() {
 
@@ -23,10 +54,6 @@ function compose_email() {
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
 
-  // Get data
-  document.querySelector('#compose-form').onsubmit = () => {
-    sent_email();
-  }
 }
 
 function load_mailbox(mailbox) {
@@ -37,27 +64,10 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Add history to the browser
+  window.history.pushState({}, "",`/`);
+  get_emails(mailbox);
 }
 
-async function sent_email(){
-    try {
-      let response = await fetch('/emails', {
-        method: 'POST',
-        body: JSON.stringify({
-          recipients: document.querySelector('#compose-recipients').value,
-          subject: document.querySelector('#compose-subject').value,
-          body: document.querySelector('#compose-body').value
-        })
-      });
-
-      const parsedEmail = await response.json();
-      console.log(parsedEmail)
-      // Load user's sent mailbox
-      load_mailbox('sent');
-
-    } catch (error) {
-      load_mailbox('archived');
-      console.log(error);
-    }
-}
 
--- mail/static/mail/sent_email.js
+++ mail/static/mail/sent_email.js
@@ -1,35 +1,63 @@
-// async function sent_email(){
-//     // Get the values
-//     try {
-//       const recipients =  document.querySelector('#compose-recipients').value;
-//       const subject = document.querySelector('#compose-subject').value;
-//       const body =  document.querySelector('#compose-body').value; 
-//       const createEmailResponse = await fetch('/emails', {
-//         method: 'POST',
-//         body: JSON.stringify({
-//           recipients: `${recipients}`,
-//           subject: `${subject}`,
-//           body: `${body}`
-//         })
-//       });
-//       const parsedEmail = await createEmailResponse.json();
-//       console.log(parsedEmail);
-//     } catch (error) {
-//       console.log(error);
-//     }
-// }
-function sent_email() {
-  fetch('/emails', {
-    method: 'POST',
-    body: JSON.stringify({
-      recipients: 'baz@example.com',
-      subject: 'Meeting time',
-      body: 'How about we meet tomorrow at 3pm?'
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Get data
+  document.querySelector("#compose-form").onsubmit = () => {
+    sent_email()
+  };
+});
+
+async function sent_email(){
+  // try {
+    await fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+        recipients: document.querySelector('#compose-recipients').value,
+        subject: document.querySelector('#compose-subject').value,
+        body: document.querySelector('#compose-body').value
+      })
     })
-  })
-    .then(response => response.json())
+    .then (response => response.json())
     .then(result => {
-      // Print result
-      console.log(result);
+
+      // Error send user back to the compose email section
+      if (result["message"] ){
+        
+        console.log("loading mailbox")
+
+        // Update url
+        window.history.pushState({}, '', 'emails/sent')
+        // Load user's sent mailbox
+        load_mailbox('sent');
+
+      } else {
+
+        console.log("in the else")
+        // Create an error message and append it to the emails-view
+        const div = document.createElement('div');
+        div.innerHTML = `<p>Error: ${result["error"]}</p>`
+        document.querySelector('#emails-view').append(div)
+
+        // Show emails view with message and hide other views
+        document.querySelector('#emails-view').style.display = 'block';
+        document.querySelector('#compose-view').style.display = 'none';
+        window.history.pushState({}, '', '/')
+        console.log(error);
+          }
     });
+
+  // } catch (error) {
+  //   console.log("in the catch")
+  //   // Create an error message and append it to the emails-view
+  //   const div = document.createElement('div');
+  //   div.innerHTML = `<p>Error: ${result["error"]}</p>`
+  //   document.querySelector('#emails-view').append(div)
+
+  //   // Show emails view with message and hide other views
+  //   document.querySelector('#emails-view').style.display = 'block';
+  //   document.querySelector('#compose-view').style.display = 'none';
+  //   console.log(error);
+  //   return false;
+  // }
+  // console.log("end")
+  return false;
 }
