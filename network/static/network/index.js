61f6397d62f3ba5a6c8ab1a1a83a91c6152a87de
Stephanie Viveros <sviveros05@gmail.com>
2020-08-26 15:42:42 -0500

    User can Edit their posts

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -1,13 +1,40 @@
 console.log("running")
 
-// document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function() {
     
-//     // Listen for buttons
-//     document.getElementById('follow').addEventListener('click',(params) => {
-//         return "todo"
-//     })
+    // Listen for which Button was clicked
+    document.querySelectorAll('button').forEach(button => {
+        button.onclick = () => {
+            if (button.dataset.action === "edit"){
+                button.innerText = "Save";
+                button.className = "btn btn-outline-primary btn-sm";
+                findPost(button);
+            } else {
+                button.dataset.action = "edit"
+                button.innerText = "Edit";
+                button.className = "btn btn-outline-secondary btn-sm";
+                const text = document.querySelector('textarea').value
+                savePost(button, text)
+            }       
+        }
+    })
     
-//     document.getElementById('unfollow').addEventListener('click',(params) => {
-//         return "todo"
-//     })
-// })
+})
+
+const findPost = (button) => {
+    document.querySelectorAll('p').forEach(p  => {
+        if (button.id === p.id){
+            p.innerHTML = `<textarea>${p.dataset.textcontent}</textarea>`;
+            button.dataset.action = "save";
+        }
+    })
+}
+
+const savePost = (button, text) => {
+    document.querySelectorAll('p').forEach(p  => {
+        if (button.id === p.id){
+            p.dataset.textcontent = text;
+            p.innerHTML = `${text}`;
+        }
+    })
+}
